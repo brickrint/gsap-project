3e5a1900637e8639257a4e9c7cd367669423f460
Rinat <brickrint@gmail.com>
2020-12-15 20:24:35 +0200

    Images reveal on hover in portfolio section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
+/* ****************************** */
+/* Navigation animations */
 function initNavigation() {
   const navLinks = gsap.utils.toArray(".main-nav a");
   const navLinksRev = gsap.utils.toArray(".main-nav a").reverse();
@@ -76,12 +78,192 @@ function moveImages(evt) {
     ease: "Power4.out",
   });
 }
+/* ****************************** */
+
+/* ****************************** */
+/* Reveal-gallery animations */
+const sections = gsap.utils.toArray(".rg__column");
+function initHoverReveal() {
+  sections.forEach((section) => {
+    section.imageBlock = section.querySelector(".rg__image");
+    section.image = section.querySelector(".rg__image img");
+    section.imageMask = section.querySelector(".rg__image--mask");
+    section.text = section.querySelector(".rg__text");
+    section.textCopy = section.querySelector(".rg__text--copy");
+    section.textMask = section.querySelector(".rg__text--mask");
+
+    // reset initial position
+    gsap.set([section.imageBlock, section.textCopy], { yPercent: -101 });
+    gsap.set([section.imageMask, section.textMask], { yPercent: 101 });
+    gsap.set(section.image, { scale: 1.2 });
+
+    // add event listeners to section
+    section.addEventListener("mouseenter", createHoverReveal);
+    section.addEventListener("mouseleave", createHoverReveal);
+  });
+}
+
+const getTextHeight = (textCopy) => textCopy.clientHeight;
+const fillBackgroundEl = document.querySelector(".fill-background");
+
+function createHoverReveal(evt) {
+  const isHovering = evt.type === "mouseenter";
+  const { imageMask, imageBlock, image, text, textCopy, textMask } = evt.target;
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.7,
+      ease: "Power4.out",
+    },
+  });
+
+  if (isHovering) {
+    tl.to([imageMask, imageBlock, textCopy, textMask], { yPercent: 0 })
+      .to(image, { duration: 1.1, scale: 1 }, 0)
+      .to(
+        text,
+        {
+          y: () => -getTextHeight(textCopy) / 2,
+        },
+        0
+      )
+      .to(fillBackgroundEl, { backgroundColor: evt.target.dataset.color }, 0);
+  } else {
+    tl.to([imageBlock, textCopy], { yPercent: -101 })
+      .to([imageMask, textMask], { yPercent: 100 }, 0)
+      .to(image, { duration: 1.1, scale: 1.2 }, 0)
+      .to(
+        text,
+        {
+          y: () => getTextHeight(textCopy) / 2,
+        },
+        0
+      );
+  }
+  return tl;
+}
+/* ****************************** */
+
+/* ****************************** */
+/* Reveal-gallery animations */
+const portfolioCategoriesContainer = document.querySelector(
+  ".portfolio__categories"
+);
+const portfolioCategories = gsap.utils.toArray(".portfolio__categories a");
+const portfoliImageContainerLarge = document.querySelector(
+  ".portfolio .portfolio__image--l"
+);
+const portfoliImageContainerSmall = document.querySelector(
+  ".portfolio .portfolio__image--s"
+);
+const portfoliImageLarge = portfoliImageContainerLarge.querySelector("div");
+const portfoliImageSmall = portfoliImageContainerSmall.querySelector("div");
+
+function initHoverPortfolio() {
+  portfolioCategories.forEach((el) => {
+    el.addEventListener("mouseenter", revealImagesOnHover);
+    el.addEventListener("mouseleave", revealImagesOnHover);
+    el.addEventListener("mousemove", createPortfoiloMove);
+  });
+}
+
+function createPortfoiloMove(evt) {
+  const { clientY } = evt;
+
+  console.log(portfolioCategoriesContainer.clientHeight);
+  console.log(clientY);
+
+  gsap.to(portfoliImageContainerLarge, {
+    duration: 1.2,
+    y: getPortfolioOffset(clientY) / 6,
+    ease: "Power3.out",
+  });
+  gsap.to(portfoliImageContainerSmall, {
+    duration: 1.5,
+    y: getPortfolioOffset(clientY) / 3,
+    ease: "Power3.out",
+  });
+}
+
+function getPortfolioOffset(clientY) {
+  return -(portfolioCategoriesContainer.clientHeight - clientY);
+}
+
+function revealImagesOnHover(evt) {
+  const el = evt.currentTarget;
+  const { imagelarge, imagesmall, color } = el.dataset;
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: "Power4.out",
+    },
+  });
+
+  if (evt.type === "mouseenter") {
+    tl.to(portfolioCategories, {
+      duration: 1,
+      color: "#fff",
+      autoAlpha: 0.2,
+    })
+      .to(el, { duration: 1, autoAlpha: 1 }, 0)
+      .set(portfoliImageLarge, { backgroundImage: `url(${imagelarge})` }, 0)
+      .set(portfoliImageSmall, { backgroundImage: `url(${imagesmall})` }, 0)
+      .to(
+        [portfoliImageContainerLarge, portfoliImageContainerSmall],
+        { autoAlpha: 1 },
+        0
+      )
+      .to(fillBackgroundEl, { backgroundColor: color, ease: "none" }, 0);
+  } else {
+    tl.to(
+      portfolioCategories,
+      { duration: 1, color: "#000", autoAlpha: 1 },
+      0
+    ).to(
+      [portfoliImageContainerLarge, portfoliImageContainerSmall],
+      { autoAlpha: 0 },
+      0
+    );
+  }
+}
+/* ****************************** */
 
 function init() {
   initNavigation();
   initNavTilt();
+
+  initHoverReveal();
+
+  initHoverPortfolio();
 }
 
-window.addEventListener("load", function () {
-  init();
-});
+// window.addEventListener("load", function () {
+//   init();
+// });
+
+const mq = window.matchMedia("(min-width: 768px)");
+
+function removeInlineStyles(...elements) {
+  if (elements.length) {
+    gsap.killTweensOf("*");
+    elements.forEach((el) => el && gsap.set(el, { clearProps: "all" }));
+  }
+}
+
+function handleWidthChange(matchMedia) {
+  if (matchMedia.matches) {
+    return init();
+  }
+  sections.forEach((section) => {
+    section.removeEventListener("mouseenter", createHoverReveal);
+    section.removeEventListener("mouseleave", createHoverReveal);
+
+    const { imageMask, imageBlock, image, text, textCopy, textMask } = section;
+    removeInlineStyles(imageMask, imageBlock, image, text, textCopy, textMask);
+  });
+}
+
+mq.addListener(handleWidthChange);
+
+handleWidthChange(mq);
